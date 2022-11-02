e5243023f593837d7ad8c9a2b3a3aaf78e36bf0a
sda607 <sda607@naver.com>
2022-11-02 18:32:54 +0900

    ItemImg

--- src/main/java/com/shop/entity/ItemImg.java
+++ src/main/java/com/shop/entity/ItemImg.java
@@ -1,4 +1,38 @@
 package com.shop.entity;
 
-public class ItemImg {
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Table(name="item_img")
+@Getter @Setter
+public class ItemImg extends BaseEntity{
+
+    @Id
+    @Column(name="item_img_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String imgName; //이미지 파일명
+
+    private String oriImgName; //원본 이미지 파일명
+
+    private String imgUrl; //이미지 조회 경로
+
+    private String repimgYn; //대표 이미지 여부
+
+    //다대일 단방향
+    //지연 로딩
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    //이미지 정보 업데이트하는 메소드
+    public void updateItemImg(String oriImgName, String imgName, String imgUrl){
+        this.oriImgName = oriImgName;
+        this.imgName = imgName;
+        this.imgUrl = imgUrl;
+    }
+
 }
